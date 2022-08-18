9a176065a1c2ef85b586bf6209fb688aa9259ee4
YahyaElgabra <yahya.m.elgabra@gmail.com>
2022-08-18 16:36:58 +0400

    deleted some redundant code

--- main.js
+++ main.js
@@ -46,39 +46,3 @@ function playRound(playerSelection, computerSelection) {
         }
     }
 }
-
-
-
-/* 
-function game() {
-    pWin = 0;
-    cWin = 0;
-    for (let i = 0; i < 5; i++) {
-        let computerSelection = getComputerChoice();
-        let playerSelection = prompt("What is your choice?", "rock, paper, or scissors?");
-        let verify = playerSelection.toLowerCase()
-        while (verify != "rock" && verify != "paper" && verify != "scissors") {
-            playerSelection = prompt("What is your choice?", "Please choose a valid option");
-        }
-
-        let round = playRound(playerSelection, computerSelection);
-
-        if (round == 0) {
-            console.log(`You won! ${playerSelection} beats ${computerSelection}`);
-            pWin += 1;
-        } else if (round == 1) {
-            console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
-            cWin += 1;
-        } else {
-            console.log(`It's a tie! You both chose ${playerSelection}`);
-        }
-    }
-    if (pWin > cWin) {
-        console.log("Congratulations! You won the match!");
-    } else if (cWin > pWin) {
-        console.log("Better luck next time");
-    } else {
-        console.log("You tied");
-    }
-}
-*/
